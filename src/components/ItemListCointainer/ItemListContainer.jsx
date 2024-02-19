b2e3f7d671f052af3c0a931c6e8dd5fbe8bcbefc
Sauret <sauret.lucas.ivan@gmail.com>
2024-02-19 03:28:47 -0300

    agregue pequeño array con lista de productos en json y lo implemente mediante fetch y map

--- src/components/ItemListCointainer/ItemListContainer.jsx
+++ src/components/ItemListCointainer/ItemListContainer.jsx
@@ -1,8 +1,46 @@
-import React from "react"
+import React,{useState,useEffect} from "react"
 
 const ItemListContainer = ({greeting}) => {
+
+    const [productos,setProductos] = useState([])
+
+    useEffect(()=>{
+        
+        const fetchData = async () => {
+            try {
+                const response = await fetch("./productos.json")
+                const data = await response.json()
+                setProductos(data)
+            }catch(error){
+                console.log("Error en el fetch "+error)
+            }
+        }
+
+        fetchData()
+
+    },[])
+
     return (
-        <><h2>{greeting}</h2></>
+        <>
+        <h2>{greeting}</h2>
+
+        {productos.length == 0 
+        ? 
+        <h1>CARGANDO...</h1> 
+        : 
+        productos.map((producto,indice)=>{
+            return(
+                <div key={producto.id}>
+                    <h2>{producto.nombre}</h2>
+                    <h2>{producto.precio}</h2>
+                    <h2>{producto.stock}</h2>
+                    <p>{producto.description}</p>
+                </div>
+            )
+        })
+        }
+        </>
+
     )
 }
 
