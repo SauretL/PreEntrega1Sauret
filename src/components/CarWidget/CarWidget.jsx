30ade087bac7e8be683baa07dbe5b2d14ce5c547
Sauret <sauret.lucas.ivan@gmail.com>
2024-03-09 23:35:22 -0300

    volvi a copiar manualmente el commit anterior y volvi a instalar vite para que pueda funcionar el proyecto en una version anterior

---
+++ src/components/CarWidget/CarWidget.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const CarWidget = () => {
+
+    return (
+        <div className="cartContainer">
+            <img src="../../../public/assets/img/carrito.png" alt="carrito" className="cart"></img>
+            <p className="cartNumber">0</p>
+        </div>
+    )
+}
+
+export default CarWidget
